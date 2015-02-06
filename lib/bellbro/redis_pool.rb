495fe7317edfea4386f0f5e49716d13372c1dfe8
Jon Stokes <jon@jonstokes.com>
2015-02-06 13:18:02 -0600

    Added a capability to bellbro

--- lib/bellbro/redis_pool.rb
+++ lib/bellbro/redis_pool.rb
@@ -1,21 +1,25 @@
 module Bellbro
-  module RedisPool
+  module Pool
     def self.included(klass)
       klass.extend(self)
-    end
 
-    def with_redis(&block)
-      retryable(sleep: 0.5) do
-        redis_pool.with &block
+      class << klass
+        def pool(conn_pool)
+          self.class_eval do
+            @connection_pool = conn_pool
+          end
+        end
       end
     end
 
-    def redis_pool
-      @redis_pool
+    def with_connection(&block)
+      retryable(sleep: 0.5) do
+        connection_pool.with &block
+      end
     end
 
-    def redis_pool=(pool)
-      @redis_pool = pool
+    def connection_pool
+      @connection_pool
     end
   end
 
