ccece650f2309902a4a778a9884fd6a4f7606a2d
Jon Stokes <jon@jonstokes.com>
2015-02-07 14:33:24 -0600

    Fix for conneciton pool class method

--- lib/bellbro/pool.rb
+++ lib/bellbro/pool.rb
@@ -1,14 +1,5 @@
 module Bellbro
   module Pool
-    def self.included(klass)
-      klass.extend(self)
-
-      class << klass
-        def pool(pool_name)
-          @pool_name = pool_name
-        end
-      end
-    end
 
     def with_connection(pool_name: nil, &block)
       retryable(sleep: 0.5) do
@@ -17,8 +8,21 @@ module Bellbro
     end
 
     def connection_pool(pool_name: nil)
-      Bellbro::Settings.connection_pools[pool_name || @pool_name]
+      self.class.connection_pool(pool_name: pool_name)
+    end
+
+    def self.included(klass)
+      klass.extend(self)
+
+      class << klass
+        def pool(pool_name)
+          @pool_name = pool_name
+        end
+
+        def connection_pool(pool_name: nil)
+          Bellbro::Settings.connection_pools[pool_name || @pool_name]
+        end
+      end
     end
   end
-
 end
