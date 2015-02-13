c061ecbb65298793cf80681dacf692a39a077a27
Jon Stokes <jon@jonstokes.com>
2015-02-13 07:43:36 -0600

    Use only sidekiq pool

--- lib/bellbro/pool.rb
+++ lib/bellbro/pool.rb
@@ -2,36 +2,43 @@ module Bellbro
   module Pool
 
     def with_connection(key: nil)
-      key ||= db_name
-      db = directory(key)
-      retryable(sleep: 0.5) do
-        Bellbro::Settings.connection_pool.with do |c|
-          c.select(db)
-          yield c
-        end
-      end
+      self.class.with_connection(key)
     end
 
     def db_name
       self.class.db_name
     end
 
-    def directory(name)
-      Bellbro::Settings.db_directory[name]
-    end
-
     def self.included(klass)
       klass.extend(self)
+      klass.extend(ClassMethods)
 
-      class << klass
-        def set_db(db_name)
-          @db_name = db_name
-        end
-        
-        def db_name
-          @db_name
+    end
+
+    def ClassMethods
+      def with_connection(key: nil)
+        key ||= db_name
+        db = directory(key)
+        retryable(sleep: 0.5) do
+          Sidekiq.redis.with do |c|
+            c.select(db)
+            yield c
+          end
         end
       end
+
+      def directory(name)
+        Bellbro::Settings.db_directory[name]
+      end
+
+      def set_db(db_name)
+        @db_name = db_name
+      end
+
+      def db_name
+        @db_name
+      end
+
     end
   end
 end
