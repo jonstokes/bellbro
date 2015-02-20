31f2a1a4f4449546f63e54c912a0d0cc2979b2e7
Jon Stokes <jon@jonstokes.com>
2015-02-20 09:09:26 -0600

    Tweak to error output

--- lib/bellbro/ringable.rb
+++ lib/bellbro/ringable.rb
@@ -3,12 +3,16 @@ module Bellbro
     def self.included(klass)
       klass.extend(self)
     end
+    
+    def gong(log_line)
+      ring(log_line, type: :error)
+    end
 
-    def ring(logline, opts={})
+    def ring(log_line, opts={})
       domain_insert = @domain ? "[#{@domain}]": ""
-      error_insert = (opts[:type] == :error) ? "##ERROR## " : ""
-      complete_logline = "[#{self.class}](#{Thread.current.object_id})#{domain_insert}: #{error_insert}#{logline}"
-      Bellbro.logger.info complete_logline
+      error_insert = (opts[:type] == :error) ? "PlatformError " : ""
+      complete_log_line = "[#{self.class}](#{Thread.current.object_id})#{domain_insert}: #{error_insert}#{log_line}"
+      Bellbro.logger.info complete_log_line
     end
   end
 end
